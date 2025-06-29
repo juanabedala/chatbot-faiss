cd9a98005fb95bd51610c3ca2820851c9f9d7557
juanabedala <68305169+juanabedala@users.noreply.github.com>
2025-06-28 23:27:13 -0300

    Update app_faiss_api.py

--- app_faiss_api.py
+++ app_faiss_api.py
@@ -18,7 +18,8 @@ METADATA_FILE = "metadata.json"
 
 # === APP FLASK ===
 app = Flask(__name__)
-CORS(app, origins=["https://chatbot-faiss-production.up.railway.app"])
+CORS(app, origins="*", supports_credentials=False, send_wildcard=True)
+# CORS(app, origins=["https://chatbot-faiss-production.up.railway.app"])
 # CORS(app, origins=["*"])   # Permite peticiones desde cualquier origen (útil si el frontend está en otro servidor)
 # CORS(app, resources={r"/consultar": {"origins": "https://www.juanico.com"}}, supports_credentials=True)
 
